42d5298c8ee67022d135f3144cd20cf564e93c2d
Tatiana <117024264+tatiansa@users.noreply.github.com>
2025-04-02 21:38:18 +0700

    Create VideoNFT.sol

---
+++ VideoNFT.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract VideoNFT is ERC721URIStorage {
+    uint256 public tokenId;
+
+    constructor() ERC721("VideoNFT", "VNF") {}
+
+    function mint() public {
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, "ipfs://bafkreidklctk3qg6zsvizon33zdpfvcyo33nidntogetfvajpymgksdsue"); // ;
+    }
+}
